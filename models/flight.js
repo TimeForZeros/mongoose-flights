53d14a0aaf5e588f9e9ed35a653f33c4e2377972
Victor Gonzalez <dazedbutamused@gmail.com>
2019-08-21 17:34:05 -0700

    starting part two

--- models/flight.js
+++ models/flight.js
@@ -2,12 +2,23 @@ var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
-var flightSchema = new Schema({
-  airline: { type: String, enum: ['United', 'Southwest', 'American']},
-  flightNo: {type: Number, min: 10, max: 9999},
-  departs: {type: Date }
+var destinationSchema = new Schema({
+  airport: {
+    type: String,
+    enum: ["AUS", "DAL", "LAX", "SEA"]
+  },
+  arrival: { type: Date }
 });
 
 
-module.exports = mongoose.model('Flight', flightSchema);
+var flightSchema = new Schema({
+  airline: { type: String, enum: ["United", "Southwest", "American"] },
+  flightNo: { type: Number, min: 10, max: 9999 },
+  departs: { type: Date },
+  airport: { type: String, enum: ["AUS", "DAL", "LAX", "SEA"], default: "SEA" },
+  destination: destinationSchema
+});
 
+
+module.exports = mongoose.model("Flight", flightSchema);
+module.exports = mongoose.model("Destination", destinationSchema);
